98898886f94bd45a6b8659567663b36f9ededb5a
Ankit <ankit90499@gmail.com>
2022-02-24 01:16:32 +0530

    feat: storing different format of content type in different cache names

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,28 @@
 const CACHE_NAME = 'test-series-product';
-// self.addEventListener('install', (e) => {
-//   e.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then((cache) => {
-//         return cache.addAll([
-//           '/',
-//           '/display-tests.html',
-//           '/css/utilities.css',
-//           '/css/display-test.css',
-//           '/css/styles.css',
-//           '/images/linkedin.png',
-//           '/scripts/display_test_archive.js',
-//           '/data/tests.json'
-//         ])
-//       })
-//   )
-// })
+// precaching on first page load
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll([
+          '/',
+          '/display-tests.html',
+          '/css/utilities.css',
+          '/css/display-test.css',
+          '/css/styles.css',
+          '/images/linkedin.png',
+          '/scripts/display_test_archive.js',
+          '/data/tests.json'
+        ])
+      })
+  )
+})
 
+// resulting the cached response if found in cache
+// or fetching over network and storing in different cache
 self.addEventListener('fetch', (event) => {
   console.log(event.request.url);
+  console.log(event.request);
   event.respondWith(
     caches.match(event.request)
       .then((response) => {
@@ -32,7 +36,7 @@ self.addEventListener('fetch', (event) => {
 
           const responseToCache = response.clone();
 
-          caches.open(CACHE_NAME)
+          caches.open(getCacheName(event.request))
             .then((cache) => {
               cache.put(event.request, responseToCache);
             });
@@ -45,4 +49,19 @@ self.addEventListener('fetch', (event) => {
 
 function isNotValidResponse(response) {
   return !response || response.status !== 200 || response.type !== 'basic'
+}
+
+function getCacheName(request) {
+  const {destination} = request;
+  switch (destination) {
+    case 'script': return 'scripts';
+    case 'style': return 'styles';
+    case 'document': return 'html';
+    case 'image': return 'images';
+    case 'object': return 'data';
+  }
+  const [contentType] = request.url.split(/\./).splice(-1);
+  if (contentType === 'json') return 'data';
+
+  return 'test';
 }
